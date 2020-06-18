2698177c324c1384ea3b5464ebdba0a6c4254e3b
Becca Staple <petite.becca.s@gmail.com>
2020-06-18 10:44:35 +0100

    now showing data - but not correct data

--- details.js
+++ details.js
@@ -7,14 +7,17 @@
     .then(res => res.json())
     .then(json => {
         json.Search.forEach(search => {
+           // console.log(search);
             const filmOut = document.getElementById("filmOutputDiv");
             const searchDiv = makeFilms("div", "", filmOut, "card w-50 border border-warning m-5");
             makeFilms("h3", `Title: ${search.Title}`, searchDiv, "card-title w-50 p-5");
             makeFilms("p", `Year: ${search.Year}`, searchDiv, "card-text p-2");
     })
-   
+}) 
+    
 
-})
+}) ();
+
 
 function makeFilms(elementType, text, appendTo, className) {
     const element = document.createElement(elementType);
